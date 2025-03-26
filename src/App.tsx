2f84e8beb5ac6f20e7f04ac0c3dcd8057db31d69
sh4d0wy <sakshambhugra8@gmail.com>
2025-03-26 12:55:32 +0530

    chore: Added react-hot-toast library in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,11 @@ function App() {
           },
           success: {
             duration: 3000,
-            theme: {
-              primary: '#4aed88',
-            },
+            
           },
           error: {
             duration: 4000,
-            theme: {
-              primary: '#ff4b4b',
-            },
+            
           },
         }}
       />
