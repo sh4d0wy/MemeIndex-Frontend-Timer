c152df92ed45913c31a57894b16ec14422eff569
sh4d0wy <sakshambhugra8@gmail.com>
2025-03-27 12:43:34 +0530

    chore: Changed styles and hover effect on buttons

--- src/Components/ConnectButton.tsx
+++ src/Components/ConnectButton.tsx
@@ -222,15 +222,15 @@ const ConnectButton = ({ onAddressChange }: ConnectButtonProps) => {
        ):(
         <>
                 <div className='flex flex-col gap-2 hover:text-red-400 cursor-pointer'>
-                    <div className='bg-white rounded-xl p-4 flex items-center justify-between hover:text-red-400 cursor-pointer'>
-                        <div className='flex items-center gap-2 text-[#0160EF]'>
+                    <div className='bg-white rounded-xl p-4 flex items-center justify-between hover:text-red-400 cursor-pointer hover:bg-white/70 text-blue-400 '>
+                        <div className='flex items-center gap-2 '>
                             <span className='font-lg font-bold'>
-                                {walletAddress ? `${walletAddress.substring(0, 6)}...${walletAddress.substring(walletAddress.length - 10)}` : ''}
+                                {walletAddress ? `${walletAddress.substring(0, 6)}.....${walletAddress.substring(walletAddress.length - 10)}` : ''}
                             </span>
                         </div>
                         <button
                             onClick={handleDisconnect}
-                            className='text-[#0160EF] hover:text-red-400 transition-colors duration-200'
+                            className=' transition-colors duration-200'
                         >
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8.5 15.5L15.5 8.49998M9 4V2M15 20V22M4 9H2M20 15H22M4.91421 4.91421L3.5 3.5M19.0858 19.0857L20.5 20.4999M12 17.6568L9.87871 19.7781C8.31662 21.3402 5.78396 21.3402 4.22186 19.7781C2.65976 18.216 2.65976 15.6833 4.22186 14.1212L6.34318 11.9999M17.6569 11.9999L19.7782 9.87859C21.3403 8.31649 21.3403 5.78383 19.7782 4.22174C18.2161 2.65964 15.6835 2.65964 14.1214 4.22174L12 6.34306" stroke="#006FFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
