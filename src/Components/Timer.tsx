17c86c80e7bb36f5370c5f2f0d9a37770d0822f6
sh4d0wy <sakshambhugra8@gmail.com>
2025-03-28 00:14:50 +0530

    chore: Changed styling of the mini app

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -63,10 +63,10 @@ interface TimeBoxProps {
 
 const TimeBox = ({ value, label }: TimeBoxProps) => {
   return (
-    <div className="flex flex-col items-center drop-shadow-xl mt-10">
-      <div className="w-12 h-12 bg-[#0d50e0] rounded-lg flex items-center justify-center 
+    <div className="flex flex-col items-center drop-shadow-xl mt-8">
+      <div className="w-18 h-18 bg-[#0d50e0] rounded-lg flex items-center justify-center 
                     shadow-[0_0_15px_rgba(0,82,255,0.5)] border border-[#0066FF]/30">
-        <span className="text-4xl font-bold text-white flex flex-col gap-1 items-center justify-center">
+        <span className="text-4xl font-bold text-white flex flex-col  items-center justify-center">
           {value.toString().padStart(2, '0')}
           <span className="text-sm text-white font-light">{label}</span>
         </span>
