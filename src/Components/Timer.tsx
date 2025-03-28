1397a5da7f450604d11bba3921b4296cdd7d5fe6
sh4d0wy <sakshambhugra8@gmail.com>
2025-03-28 09:42:51 +0530

    chore: Changed styles and margin between components

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -64,7 +64,7 @@ interface TimeBoxProps {
 const TimeBox = ({ value, label }: TimeBoxProps) => {
   return (
     <div className="flex flex-col items-center  w-full">
-      <div className="w-1/4 h-18 bg-[#002dfe] rounded-lg flex items-center justify-center 
+      <div className="w-full flex-1/4 h-18 bg-[#002dfe] rounded-lg flex items-center justify-center 
                     shadow-[0_0_15px_rgba(0,82,255,0.5)] border border-[#0066FF]/30">
         <span className="text-3xl font-[800] text-white flex flex-col  items-center justify-center">
           {value.toString().padStart(2, '0')}
