e8ab0547092cda091184bc5dd8451233b8c25765
sh4d0wy <sakshambhugra8@gmail.com>
2025-04-01 19:47:26 +0530

    chore: Added analytics script

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import TelegramAnalytics from '@telegram-apps/analytics'
 
+TelegramAnalytics.init({
+    token: 'eyJhcHBfbmFtZSI6Ik1lbWVCYXR0bGVBcmVuYSIsImFwcF91cmwiOiJodHRwczovL3QubWUvTWVtZUJhdHRsZUFyZW5hQm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vYXJlbmEubWlkZXguaW8vIn0=!qm7E5V4l3SplbE6Zkyx/rioXQ2EUE8aUeM5EuhYBNbo=',
+    appName: 'MemeBattleArena',
+});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
