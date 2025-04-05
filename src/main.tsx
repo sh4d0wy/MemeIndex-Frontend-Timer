f7dcf52b0dda4aad94442008f844a422d61ea44b
sh4d0wy <sakshambhugra8@gmail.com>
2025-04-05 10:44:05 +0530

    chore: Added env variables for datachief

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import App from './App.tsx'
 import TelegramAnalytics from '@telegram-apps/analytics'
 
 TelegramAnalytics.init({
-    token: 'eyJhcHBfbmFtZSI6Ik1lbWVCYXR0bGVBcmVuYSIsImFwcF91cmwiOiJodHRwczovL3QubWUvTWVtZUJhdHRsZUFyZW5hQm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vYXJlbmEubWlkZXguaW8vIn0=!qm7E5V4l3SplbE6Zkyx/rioXQ2EUE8aUeM5EuhYBNbo=',
-    appName: 'MemeBattleArena',
+  token: import.meta.env.VITE_ANALYTICS_TOKEN,
+  appName: import.meta.env.VITE_ANALYTICS_APP_NAME,
 });
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
